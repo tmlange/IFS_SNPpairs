3f614eddd594a5bc9353d840fea302fe197f0fd7
tmlange <80765465+tmlange@users.noreply.github.com>
2023-02-03 12:58:03 +0100

    Update IFS_SNPpairs.R

--- IFS_SNPpairs.R
+++ IFS_SNPpairs.R
@@ -26,7 +26,29 @@ max.snps = 20
 
 # Read in the function create_SNPpairs()
 # If the R script create_SNPpairs.R is in the working directory, run the following line
-source("create_SNPpairs.R")
+create_SNPpairs = function(selectedSNPs, D){
+  
+  # Combining the single SNPs to SNP pairs
+  comb = combn(selectedSNPs, 2)
+  comb = as.data.frame(comb)
+  comb = t(comb)
+  
+  
+  # Defining the genotype and phenotype of the SNP pairs with the provided data set
+  D_SNPpairs = D[,1]
+  D_SNPpairs = as.data.frame(D_SNPpairs)
+  names(D_SNPpairs) = "PHENOTYPE"
+  for(i in 1:nrow(comb)){
+    geno_SNP1 = D[,comb[i,1]]
+    geno_SNP2 = D[,comb[i,2]]
+    geno_SNPpair = geno_SNP1*geno_SNP2
+    
+    D_SNPpairs = cbind(D_SNPpairs, geno_SNPpair)
+    names(D_SNPpairs)[i+1] = paste(comb[i,1], comb[i,2], sep= "_")
+  }
+  
+  return(D_SNPpairs)
+}
 
 
 
